ebfbb5666d1d3fde255482dfbed4af39df62cf02
nemeremfundz-ux <nemeremfundz@gmail.com>
2026-07-30 22:26:08 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,10 @@
 const SUPABASE_URL = "https://ruawhppwaxfqpiiiscnhy.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_H0Q8ciikBPDxpd7bhHLJzg_S4-7MNK_";
 
-const { createClient } = supabase;
+const { createClient } = window.supabase;
 
 const supabaseClient = createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+const supabase = supabaseClient;
